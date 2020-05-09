eeb783e8b6ab05891dfd1057358f4a515d6f5326
AA <adi.2002.gwl@gmail.com>
2020-05-09 12:05:25 +0530

    first commit

--- clientg.js
+++ clientg.js
@@ -7,7 +7,7 @@ form.addEventListener("submit",(e)=> {
     const name = formData.get("name");
 
     const payload = {
-        name : name
+        name
     }
 
     fetch(L_URL, {
@@ -18,31 +18,65 @@ form.addEventListener("submit",(e)=> {
         }
     });
     e.preventDefault();
+    formData = '';
+
+    e.preventDefault();
 });
 
-fetch(L_URL)
-.then((response)=> {
-    return response.json();
-})
-.then((myJson) => {
-    console.log(myJson.length);
-    for(let i =0; i<myJson.length; i++ ){
-        var  new_entry = document.createElement("tr");
+getData();
+function getData(){
 
-        var name = document.createElement("td");
-        name.id = "name";
-        var name_node = document.createTextNode(myJson[i].name);
-        name.appendChild(name_node);
-        new_entry.appendChild(name);
+    const tbody = document.getElementById("genre-d");
+    tbody.innerHTML = '';
 
-        var date = document.createElement("td");
-        date.id = "date";
-        var date_node = document.createTextNode(myJson[i].create_date);
-        date.appendChild(date_node);
-        new_entry.appendChild(date);
-        
 
-        var table_row = document.getElementById('genre-list');
-        table_row.appendChild(new_entry);
-    }
-})
+    fetch(L_URL)
+    .then((response)=> {
+        return response.json();
+    })
+    .then((myJson) => {
+        console.log(myJson.length);
+        for(let i =0; i<myJson.length; i++ ){
+            var  new_entry = document.createElement("tr");
+
+            var name = document.createElement("td");
+            name.id = "name";
+            var name_node = document.createTextNode(myJson[i].name);
+            name.appendChild(name_node);
+            new_entry.appendChild(name);
+            new_entry.id = i.toString();
+
+            var date = document.createElement("td");
+            date.id = "date";
+            var date_node = document.createTextNode(myJson[i].create_date);
+            date.appendChild(date_node);
+            new_entry.appendChild(date);
+            
+            var del = document.createElement("td");
+            var del_btn = document.createElement("button");
+            var btn_node = document.createTextNode("X");
+            del_btn.id = "del-"+i.toString();
+            del_btn.appendChild(btn_node);
+            del.appendChild(del_btn);
+            new_entry.appendChild(del);
+            var table_row = document.getElementById('genre-d');
+            table_row.appendChild(new_entry);
+
+            document.getElementById(`del-${i}`).addEventListener("click", (e) => {
+                const td = e.target.parentElement;
+                const tr = td.parentElement;;
+                const name = tr.firstElementChild.innerText;
+                fetch(`L_URL${name}`, {
+                    method: "DELETE",
+                    headers: {
+                        'content-type': 'application/json'
+                    }
+                })
+                .then(res => console.log(res));
+                tr.remove();
+            });
+
+        };
+    });
+
+}
